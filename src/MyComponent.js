ced7cceb9d3445f8b8188262aba485d49f616c56
PabloMartinezA <99064319+PabloMartinezA@users.noreply.github.com>
2023-05-30 15:59:32 -0700

    Add files via upload

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -3,6 +3,7 @@ import { Line } from 'react-chartjs-2';
 
 const MyComponent = () => {
   const [data, setData] = useState([]);
+  console.log('Received data:', data)
 
   useEffect(() => {
     fetchData();
@@ -13,21 +14,28 @@ const MyComponent = () => {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        'Cache-Control': 'no-cache', // Agrega esta cabecera para evitar la caché
       },
+      body: JSON.stringify({}),
     })
-      .then((response) => response.json())
+      .then((response) => {
+        console.log('Received response:', response);
+        return response.json();
+      })
       .then((data) => {
         const ledValue = data.ledValue;
-        setData((prevData) => [...prevData, ledValue]);
-        setTimeout(fetchData, 5000);
+        setData((prevData) => [...prevData.slice(-9), ledValue]);
+        setTimeout(fetchData, 20000);
       })
       .catch((error) => {
         console.error('Error al obtener los datos:', error);
       });
   };
+  
+  
 
   const chartData = {
-    labels: data.map((_, index) => `Dato ${index + 1}`),
+    labels: data.length > 0 ? data.map((_, index) => `Dato ${index + 1}`) : [],
     datasets: [
       {
         label: 'Valor del LED',
--- src/app.py
+++ src/app.py
@@ -2,18 +2,21 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
-@app.route('/data', methods=['POST'])
+@app.route('/data', methods=['POST', 'OPTIONS'])
 def get_data():
-    data = request.get_json()
-    led_value = data.get('ledValue', 0.0)
+    if request.method == 'OPTIONS':
+        return jsonify({'status': 'success'})
 
+    data = request.get_json()
+    print('Received data:', data)  # Agrega este mensaje de depuración
+    led_value = data.get('ledValue', 0)
+    print('Debug2:', data)
     response = {'ledValue': led_value}
-    print(response)
+    print('Response:', response)  # Agrega este mensaje de depuración
 
     return jsonify(response)
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)  # Agrega debug=True para obtener información detallada en caso de errores
